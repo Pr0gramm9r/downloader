dd2437aa3218e2f55d9eb2ec48445d5968e518aa
majorik <krashbox3@gmail.com>
2026-08-25 12:25:35 +0300

    Delete download_project directory

--- download_project/handlers/states.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class DownloadStates(StatesGroup):
-    waiting_for_link = State() #статус ожидания ссылки
-    waiting_for_quality = State() #статус ожидания качества (видео)
